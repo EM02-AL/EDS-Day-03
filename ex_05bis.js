9192eaf9033a739ff99ca05168bafc081c74fde1
EM02-AL <eddy.mazi@epitech.digital>
2024-09-04 11:53:55 +0200

    Update ex_05bis.js

--- ex_05bis.js
+++ ex_05bis.js
@@ -1,12 +1,17 @@
 function getBasketContent(selectedFruits) {
 const fruits = ["straberry", "straberry", "straberry", "apple", "apple", "lime", "lime", "peach", "pear", "pear"]; 
+
 if (selectedFruits > fruits.lenght) {
 console.log ("Too many fruit(s)selected.")
 return []
-} else {console.log(fruits.length + "fruit(s) selected"); 
+} 
+console.log(fruits.length + "fruit(s) selected"); 
 return fruits.splice(0, selectedFruits)
 }
 
-return fruits;
-}
  
+
+if (typeof getBasketContent === "function") {
+	const fruits = getBasketContent()
+	showMyBasket(fruits)
+}

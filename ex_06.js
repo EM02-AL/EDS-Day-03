415ba72101944cabddaf2616b6ed94f09646868f
EM02-AL <eddy.mazi@epitech.digital>
2024-09-04 12:36:06 +0200

    Update ex_06.js

--- ex_06.js
+++ ex_06.js
@@ -1,1 +1,10 @@
+fruits = ["Blackberries", "Apple", "Banana", "Mango", "Peaches", "Kiwi"];
 
+function findFruitIndex(fruitName) {
+if (fruits.indexOf(fruitName) == -1) 
+return null
+
+return fruits.indexOf(fruitName) ;
+}
+
+displayResult(findFruitIndex("Banana")) 
